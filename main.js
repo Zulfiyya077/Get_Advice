9c0733dc18c4b6dcbd0d1e20d821bc2b28cbb3d1
Zulfiyya Mammadli <mammadli.zulfiyya07@gmail.com>
2025-08-09 18:03:35 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let advice = document.querySelector('.advice');
+let id = document.querySelector('.idnum');
+let btn = document.querySelector('.btn');
+let sound = document.querySelector('.sound');
+let heart = document.querySelector('.heart');
+
+const url = 'https://api.adviceslip.com/advice';
+
+let getAdvice = () => {
+    fetch(url).then((data) => data.json()).then((item) => {
+        advice.innerHTML = item.slip.advice;
+        id.innerHTML = item.slip.id;
+    }).catch((error) => {
+        console.log( error);
+
+    });
+};
+
+sound.addEventListener('click', () => {
+    let sound = new SpeechSynthesisUtterance(`${advice.innerHTML}`);
+    speechSynthesis.speak(sound);
+});
+
+function myFunction(x) {
+    const isFilled = x.classList.contains("fa-heart");
+    
+    if (isFilled) {
+       
+        x.classList.replace("fa-heart", "fa-heart-o");
+    } else {
+      
+        x.classList.replace("fa-heart-o", "fa-heart");
+    }
+}
+
+window.addEventListener('load', () => {
+    getAdvice();
+});
+
+btn.addEventListener('click', () => {
+    getAdvice();
+});
+
+heart.addEventListener('click', () => {
+    myFunction(heart);
+});
